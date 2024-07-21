b40911f2038f12be893ea92c368f528044ca75e4
aleenarose222 <aleenarose222@gmail.com>
2024-07-21 20:03:09 +0530

    first commit

---
+++ javas.js
@@ -0,0 +1,14 @@
+function btclick(val)
+{
+    document.getElementById("screen").value+=val;
+}
+function clearScreen()
+{
+    document.getElementById("screen").value="";
+}
+function equalclick()
+{
+    var text=document.getElementById("screen").value
+    var result=eval(text)
+    document.getElementById("screen").value=result
+}
